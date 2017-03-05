b4487f420afd96f2c6cf21f5a9f91225d777760d
sach <sacha.saint-leger@mail.mcgill.ca>
2017-03-04 20:06:27 -0500

    edits displayed on globe as expected

--- draw.js
+++ draw.js
@@ -50,41 +50,92 @@ function draw() {
 			c.lineWidth= 0.5, c.shadowBlur = 1, c.shadowColor="lightgrey", c.beginPath(), path(globe), c.stroke();
 		    };
 		});
-    });
 
-    var url = 'https://stream.wikimedia.org/v2/stream/recentchange';
+	var url = 'https://stream.wikimedia.org/v2/stream/recentchange';
+	var edits = [];
+	var queue = [];
 
-    console.log('Connecting to EventStreams at ' + url);
-    var eventSource = new EventSource(url);
+	console.log('Connecting to EventStreams at ' + url);
+	var eventSource = new EventSource(url);
 
-    eventSource.onopen = function(event) {
-	console.log('--- Opened connection.');
-    };
-
-    eventSource.onerror = function(event) {
-	console.error('--- Encountered error', event);
-    };
-
-    eventSource.onmessage = function(event) {
-	// event.data will be a JSON string containing the message event.
-	var dict = JSON.parse(event.data);
-	var user = dict.user;
-	
-	if (isIPaddress(user)) {
-	    var url = "http://freegeoip.net/json/" + user;
-	    //convert ip address to geolocation (lat, lon coordinates)
-	    //note while this is ok for development, there is a limit of 15000 requests
-	    //per hour -> will need to deploy own instance of freegeoip web server to heroku
-	    //for production
-	    //see https://github.com/fiorix/freegeoip on how to do this
-	    getJSON(url, function(err,data) {
-		lat = data.latitude;
-		lon = data.longitude;
-		console.log("lat : " + lat + " , " + "lon : " + lon);
-		return [lat,lon];
-	    });
+	eventSource.onopen = function(event) {
+	    console.log('--- Opened connection.');
 	};
-    };
+
+	eventSource.onerror = function(event) {
+	    console.error('--- Encountered error', event);
+	};
+
+	eventSource.onmessage = function(event) {
+	    // event.data will be a JSON string containing the message event.
+	    var dict = JSON.parse(event.data);
+	    var user = dict.user;
+
+	    if (isIPaddress(user)) {
+		var url = "http://freegeoip.net/json/" + user;
+		//convert ip address to geolocation (lat, lon coordinates)
+		//note while this is ok for development, there is a limit of 15000 requests
+		//per hour -> will need to deploy own instance of freegeoip web server to heroku
+		//for production
+		//see https://github.com/fiorix/freegeoip on how to do this
+		getJSON(url, function(err,data) {
+		    lat = data.latitude;
+		    lon = data.longitude;
+		    data.latLong = [lon,lat]
+//		    console.log("lat : " + lat + " , " + "lon : " + lon);
+		    queue.push(data)
+		});
+	    };
+	};
+	//every 5 seconds check queue array to see if you have received an edit,
+	//if you have, remove it from queue array, add its coordinates to map
+	setInterval(function() {
+	    var edit = queue.shift();
+	    if (edit) {
+		edits.push(edit.latLong);
+		d3.transition()
+		    .duration(3000)
+		    .tween("rotate", function() {
+			var p = edit.latLong,
+				r = d3.interpolate(projection.rotate(), [-p[0], -p[1]]);
+			    return function(t) { //t refers to time spent tweening so far
+				// Rotate the earth so that the new point is front and center.
+				projection.rotate(r(t));
+				// Erase the canvas.
+				c.clearRect(0, 0, width, height);
+				// Color in land masses.
+				c.fillStyle = countryFill,
+				c.beginPath(),
+				path(land),
+				c.fill();
+				// Draw the country borders.
+				c.strokeStyle = "white",
+				c.lineWidth = 1,
+				c.beginPath(),
+				path(borders),
+				c.stroke();
+				// Draw the earth's circumference
+				c.lineWidth=0.5,
+				c.shadowBlur = 1,
+				c.shadowColor="lightgrey",
+				c.beginPath(),
+				path(globe),
+				c.stroke();
+				// Get the canvas-coordinates of the latLong points associated with the latest edit we have
+				//received , and draw a circle there.
+				var center = projection(p);
+				// draw the circle associated with the latest edit slightly bigger
+				c.fillStyle = "orange",
+				c.beginPath(),
+				c.arc(center[0], center[1],5, 0, 2 * Math.PI, false),
+				c.lineWidth = 0.5,
+				c.fill(),
+				c.stroke();
+			    } 
+		    });
+	    }
+	}, 5000);
+    });
 
     function getJSON(url, callback) {
 	var xhr = new XMLHttpRequest();
